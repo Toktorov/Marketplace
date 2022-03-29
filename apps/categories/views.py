a3ad43097bcfc1f08c58c6646c72b2b24f34ab1f
Toktorov <ktoktorov2005@gmail.com>
2022-03-29 17:25:23 +0600

    pagination

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from apps.categories.models import Category
 from apps.products.models import Product
 from apps.settings.models import Setting
+from django.core.paginator import Paginator
 
 # Create your views here.
 def category_detail(request, slug):
@@ -9,10 +10,14 @@ def category_detail(request, slug):
     categories = Category.objects.all().order_by('?')[:5]
     home = Setting.objects.latest('-id')
     products = Product.objects.all().order_by('-id')
+    paginator = Paginator(products, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'category' : category,
         'products' : products,
         'home' : home,
         'categories' : categories,
+        'page_obj': page_obj,
     }
     return render(request, 'category_detail.html', context)
