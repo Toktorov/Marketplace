683022a45552d88ce8874aada08e97872c808ae1
Toktorov <ktoktorov2005@gmail.com>
2022-04-02 16:28:26 +0600

    search page

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from apps.products.views import product_detail
+from apps.products.views import product_detail, product_search
 
 
 urlpatterns = [
     path('product/<int:id>', product_detail, name = "product_detail"),
+    path('search/', product_search, name = "product_search"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from apps.products.models import Product
 from apps.settings.models import Setting
 from apps.categories.models import Category
+from django.db.models import Q
 
 # Create your views here.
 def product_detail(request, id):
@@ -16,3 +17,15 @@ def product_detail(request, id):
         'categories' : categories,
     }
     return render(request, 'product-left-sidebar.html', context)
+
+def product_search(request):
+    products = Product.objects.all()
+    qury_obj = request.GET.get('key')
+    home = Setting.objects.latest('id')
+    if qury_obj:
+        products = Product.objects.filter(Q(title__icontains = qury_obj))
+    context = {
+        'home' : home, 
+        'products' : products
+    }
+    return render(request, 'products/search.html', context)
