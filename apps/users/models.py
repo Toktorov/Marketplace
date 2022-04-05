56bd62d21765a0d0eb5d7a7f8c6d72994ad822ea
Toktorov <ktoktorov2005@gmail.com>
2022-04-05 16:43:59 +0600

    user model

---
+++ apps/users/models.py
@@ -0,0 +1,14 @@
+from tabnanny import verbose
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    tel = models.CharField(max_length=255, default="0", blank=True, null=True)
+
+    def __str__(self):
+        return self.username 
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'apps.categories',
     'apps.products',
     'apps.settings',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ MEDIA_ROOT= BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
