06adae209f594839871d9064ce06b85b178620d5
Toktorov <ktoktorov2005@gmail.com>
2022-04-26 17:23:14 +0600

    admin static

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,7 +70,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -169,8 +170,8 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
-STATICFILES_DIRS= [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR / '/static/')
+STATICFILES_DIRS= [os.path.join(BASE_DIR / 'static')]
 
 
 MEDIA_URL = '/media/'
